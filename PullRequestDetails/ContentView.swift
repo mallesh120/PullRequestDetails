317b14c9bbf85650b5f34aac95fb06bf0fc8ceb2
Mallesh Kumar <mallesh120@gmail.com>
2020-10-11 03:02:40 -0400

    Base Project with Swift UI

---
+++ PullRequestDetails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PullRequestDetails
+//
+//  Created by Tanikella, Sai Mallesh Kum  (S.) on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PullRequestDetails/PullRequestDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullRequestDetailsApp.swift
+//  PullRequestDetails
+//
+//  Created by Tanikella, Sai Mallesh Kum  (S.) on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PullRequestDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
